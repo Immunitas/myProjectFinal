db92f64102d4020524f31d3ceaf02325a55b1145
ziva05 <zivtav@gmail.com>
2022-06-10 12:45:09 +0200

    update horloge & CSS & html

--- static/script.js
+++ static/script.js
@@ -1,1 +1,12 @@
 localStorage.setItem("myProject", "Ziva")
+
+function Timer() {
+    var dt=new Date()
+    document.getElementById("horloge").innerHTML=dt.getHours()+":"+dt.getMinutes()+":"+dt.getSeconds();
+    setTimeout("Timer()",900);
+      console.log("Exécution de Timer()");
+    
+}
+Timer();
+
+ 
